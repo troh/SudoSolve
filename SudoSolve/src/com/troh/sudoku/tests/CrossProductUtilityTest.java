98adecca00440388209b7d08f39bd4248dbc59f7
tom <tohaga@outlook.com>
2013-11-21 03:20:48 +0000

    Added initial test classes.

---
+++ SudoSolve/src/com/troh/sudoku/tests/CrossProductUtilityTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.troh.sudoku.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author tom
+ *
+ */
+public class CrossProductUtilityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ SudoSolve/src/com/troh/sudoku/tests/GridStructureTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.troh.sudoku.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author tom
+ *
+ */
+public class GridStructureTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
