4dfcdc3654e27296ecffd50a8961f15a52c0b6d4
tom <tohaga@outlook.com>
2013-11-22 10:45:30 +0000

    Added extra test.

--- SudoSolve/src/com/troh/sudoku/tests/SolverStrategyTest.java
+++ SudoSolve/src/com/troh/sudoku/tests/SolverStrategyTest.java
@@ -15,10 +15,12 @@ import com.troh.sudoku.main.NorvigSolverStrategy;
 import com.troh.sudoku.main.SolverStrategy;
 
 public class SolverStrategyTest {
+	private String easyPuzzle1 = "003020600900305001001806400008102900700000008006708200002609500800203009005010300";
 	private String hardPuzzle1 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......";
 	private String hardPuzzle2 = "52...6.........7.13...........4..8..6......5...........418.........3..2...87.....";
-	private String solution1 = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-	private String solution2 = "527316489896542731314987562172453896689271354453698217941825673765134928238769145";
+	private String easySolution1 = "483921657967345821251876493548132976729564138136798245372689514814253769695417382";
+	private String hardSolution1 = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	private String hardSolution2 = "527316489896542731314987562172453896689271354453698217941825673765134928238769145";
 	private SolverStrategy norvigSolve;
 	private GridStructure gridStructure;
 	
@@ -30,15 +32,14 @@ public class SolverStrategyTest {
 	
 	@Test
 	public void testHardPuzzle1() {
-		Grid result = norvigSolve.solve(hardPuzzle1);
-		assertTrue(compareSignatureWithGrid(solution1, result));
+		Grid result1 = norvigSolve.solve(hardPuzzle1);
+		assertTrue(compareSignatureWithGrid(hardSolution1, result1));
+		Grid result2 = norvigSolve.solve(hardPuzzle2);
+		assertTrue(compareSignatureWithGrid(hardSolution2, result2));
+		Grid result3 = norvigSolve.solve(easyPuzzle1);
+		assertTrue(compareSignatureWithGrid(easySolution1, result3));
 	}
 	
-	@Test
-	public void testHardPuzzle2() {
-		Grid result = norvigSolve.solve(hardPuzzle2);
-		assertTrue(compareSignatureWithGrid(solution2, result));
-	}
 	
 	private boolean compareSignatureWithGrid(String signature, Grid grid) {
 		List<String> squares = gridStructure.getSquares();
